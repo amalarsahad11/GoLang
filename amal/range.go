86eea8ebc5542067a036bbf30818b299c3e8abef
amal <arsadamal@gmail.com>
2023-06-26 12:59:20 +0530

    range

---
+++ amal/range.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var data = [4]int{11, 12, 13, 14}
+	for _, value := range data {
+		fmt.Println(value)
+	}
+	var sdata = make([]int, 3)
+	sdata[0] = 15
+	sdata[1] = 16
+	sdata[2] = 16
+	for _, value := range sdata {
+	}
+}
