c1232c18d94c58337f17f1ee49e72788f21abf67
amal arshad <107812522+amalarsahad11@users.noreply.github.com>
2023-06-26 15:26:53 +0530

    Add files via upload

---
+++ amal/Varaibles.go
@@ -0,0 +1,17 @@
+//varaibles
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var num1, num2 int
+	num1, num2 = 5, 17
+	var result = num1 + num2
+	fmt.Println(result)
+	a := "grapes"
+	fmt.Println(a)
+	var y = "golang"
+	fmt.Println(y)
+}
